a5c1d313701721636ae0294ea98df02ea8b02518
Jairm245 <jairm245@gmail.com>
2026-08-25 17:13:55 -0700

    connected app to supabase

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,47 @@
+import { router } from "expo-router";
+import { useState } from "react";
+import { Button, Pressable, Text, TextInput, View } from "react-native";
+import { supabase } from "../../lib/supabase";
+
+
+export default function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState<string | null>(null);
+const handleLogIn = async() => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+    });
+
+    if (error){
+        setError(error.message);
+        return;
+    }
+    console.log("Login successful:", data);
+    router.replace("/(tabs)");
+};
+
+return (
+    <View>
+        <Text>Login</Text>
+        <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        />
+        <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        />
+        {error && <Text style={{ color: "red" }}>{error}</Text>}
+        <Button title="Log In" onPress={handleLogIn} />
+        <Pressable onPress={() => router.push("/(auth)/signup")}>
+            <Text>Don't have an account? Sign Up</Text>
+        </Pressable>
+    </View>
+)
+}
---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,61 @@
+import { router } from "expo-router";
+import { useState } from "react";
+import { Button, Pressable, Text, TextInput, View } from "react-native";
+import { supabase } from "../../lib/supabase";
+
+export default function SignupScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    const [message, setMessage] = useState<string | null>(null);
+    
+    const handleSignup = async () => {
+        setError(null);
+        setMessage(null);
+
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+        });
+
+        if (error) {
+            setError(error.message);
+            return;
+        } 
+        console.log("Signup successful:", data);
+        if(!data.session) {
+            setMessage("Check your email to confirm your account.")
+        }
+        
+    };
+
+    return (
+        <View>
+            <Text>
+                Sign Up
+            </Text>
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+            />
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+
+            {error && <Text style={{ color: "red" }}>{error}</Text>}
+            {message && <Text style={{ color: "green" }}>{message}</Text>}
+
+
+            <Button title="Create Account"
+             onPress={handleSignup} />
+             <Pressable onPress={() => router.replace("/(auth)/login")}>
+                <Text>Already have an account? Log In</Text>
+             </Pressable>
+        </View>
+    );
+}
